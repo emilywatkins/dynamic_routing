f0c3c9e7abe6fba94d2a68d1408c80c0cc462366
Alex Ficklin and Emily Watkins <emily@>
2018-02-14 09:31:47 -0800

    initialize files and logic for dynamic routing with sinatra.

---
+++ lib/item.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/ruby
+
+class Item
+  @@list = []
+
+  attr_accessor :name
+  attr_reader :id
+
+  def initialize(name)
+    @name = name
+    @id = @@list.length + 1
+  end
+
+  def self.all()
+    @@list
+  end
+
+  def save()
+    @@list.push(self)
+  end
+
+  def self.clear()
+    @@list = []
+  end
+
+  def self.find(id)
+    item_id = id.to_i()
+    @@list.each do |item|
+      if item.id == item_id
+        return item
+      end
+    end
+  end
+end
